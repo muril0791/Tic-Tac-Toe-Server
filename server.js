3b72da1646ecd5b011f103cc06bc06950089f680
Murilo Ferreira <muriloferreira.santos1@gmail.com>
2024-06-18 17:44:18 -0300

    Change url

--- server.js
+++ server.js
@@ -6,16 +6,18 @@ import { nanoid } from "nanoid";
 
 const app = express();
 const server = http.createServer(app);
+
+const clientUrl = process.env.CLIENT_URL || "http://localhost:5173";
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:5173", // Substitua pela URL do seu cliente
+    origin: clientUrl,
     methods: ["GET", "POST"],
   },
 });
 
 app.use(
   cors({
-    origin: "http://localhost:5173", // Substitua pela URL do seu cliente
+    origin: clientUrl,
   })
 );
 
