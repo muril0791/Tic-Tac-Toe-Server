cb1b399987f99cef1ea0bb5befb89956a4e2b31a
Murilo Ferreira <muriloferreira.santos1@gmail.com>
2024-06-19 12:22:48 -0300

    changes to run server on vercel

--- server.js
+++ server.js
@@ -73,22 +73,30 @@ app.get("/logs", basicAuth, (req, res) => {
   });
 });
 
+app.get("/", (req, res) => {
+  res.send("Backend is running");
+});
+
 io.on("connection", (socket) => {
   console.log("a user connected:", socket.id);
 
   socket.on("login", (username) => {
+    console.log(`Login attempt by ${username}`);
     if (
       !players[socket.id] &&
       (username === "Player1" || username === "Player2")
     ) {
       players[socket.id] = { username };
       socket.emit("logged_in", { username, rooms });
+      console.log(`User ${username} logged in with ID: ${socket.id}`);
     } else {
       socket.emit("login_error", "Invalid username or already logged in.");
+      console.log(`Login error for user ${username}`);
     }
   });
 
   socket.on("create_room", (roomName) => {
+    console.log(`Create room attempt: ${roomName}`);
     if (!rooms[roomName]) {
       rooms[roomName] = {
         id: nanoid(),
@@ -101,10 +109,12 @@ io.on("connection", (socket) => {
         playAgainVotes: 0,
       };
       io.emit("room_list", rooms);
+      console.log(`Room created: ${roomName}`);
     }
   });
 
   socket.on("join_room", (roomName) => {
+    console.log(`Join room attempt: ${roomName}`);
     if (rooms[roomName] && Object.keys(rooms[roomName].players).length < 2) {
       rooms[roomName].players[socket.id] = {
         symbol: Object.keys(rooms[roomName].players).length === 0 ? "X" : "O",
@@ -112,6 +122,7 @@ io.on("connection", (socket) => {
       };
       socket.join(roomName);
       io.to(roomName).emit("room_update", rooms[roomName]);
+      console.log(`User joined room: ${roomName}`);
       if (Object.keys(rooms[roomName].players).length === 2) {
         startGameCountdown(roomName);
       }
@@ -119,6 +130,7 @@ io.on("connection", (socket) => {
   });
 
   socket.on("move", ({ roomName, index }) => {
+    console.log(`Move attempt in room ${roomName}, index ${index}`);
     const room = rooms[roomName];
     if (
       room &&
@@ -127,6 +139,7 @@ io.on("connection", (socket) => {
     ) {
       room.board[index] = room.players[socket.id].symbol;
       io.to(roomName).emit("board_update", room.board);
+      console.log(`Move registered in room ${roomName}, index ${index}`);
 
       const winner = checkWin(room.board);
       if (winner) {
@@ -134,21 +147,27 @@ io.on("connection", (socket) => {
         io.to(roomName).emit("game_end", { winner });
         room.board = Array(9).fill(null);
         room.inProgress = false;
+        console.log(`Game end in room ${roomName}, winner: ${winner}`);
       } else if (checkDraw(room.board)) {
         room.history.push({ board: [...room.board], winner: null });
         io.to(roomName).emit("game_end", { winner: null });
         room.board = Array(9).fill(null);
         room.inProgress = false;
+        console.log(`Game end in room ${roomName}, draw`);
       } else {
         room.currentPlayer = Object.keys(room.players).find(
           (playerId) => playerId !== socket.id
         );
         io.to(roomName).emit("turn_update", room.currentPlayer);
+        console.log(
+          `Turn update in room ${roomName}, current player: ${room.currentPlayer}`
+        );
       }
     }
   });
 
   socket.on("play_again", (roomName) => {
+    console.log(`Play again attempt in room ${roomName}`);
     const room = rooms[roomName];
     if (room) {
       room.playAgainVotes += 1;
@@ -159,13 +178,18 @@ io.on("connection", (socket) => {
         io.to(roomName).emit("board_update", room.board);
         io.to(roomName).emit("reset_game");
         startGameCountdown(roomName);
+        console.log(`Game reset in room ${roomName}`);
       } else {
         io.to(roomName).emit("wait_for_play_again");
+        console.log(
+          `Waiting for other player to play again in room ${roomName}`
+        );
       }
     }
   });
 
   socket.on("send_message", ({ roomName, message }) => {
+    console.log(`Message sent in room ${roomName}: ${message}`);
     const room = rooms[roomName];
     if (room) {
       const player = players[socket.id].username;
@@ -175,19 +199,23 @@ io.on("connection", (socket) => {
   });
 
   socket.on("leave_room", (roomName) => {
+    console.log(`Leave room attempt: ${roomName}`);
     const room = rooms[roomName];
     if (room) {
       delete room.players[socket.id];
       socket.leave(roomName);
       io.to(roomName).emit("room_update", room);
+      console.log(`User left room: ${roomName}`);
       if (Object.keys(room.players).length === 0) {
         delete rooms[roomName];
         io.emit("room_list", rooms);
+        console.log(`Room deleted: ${roomName}`);
       }
     }
   });
 
   socket.on("disconnect", () => {
+    console.log(`User disconnected: ${socket.id}`);
     for (const roomName in rooms) {
       const room = rooms[roomName];
       if (room.players[socket.id]) {
@@ -196,12 +224,12 @@ io.on("connection", (socket) => {
         if (Object.keys(room.players).length === 0) {
           delete rooms[roomName];
           io.emit("room_list", rooms);
+          console.log(`Room deleted: ${roomName}`);
         }
         break;
       }
     }
     delete players[socket.id];
-    console.log("user disconnected:", socket.id);
   });
 });
 
